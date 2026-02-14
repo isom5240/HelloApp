f64d1e79dd8172d47afeaba9fb7e33ae75a8d4fe
isom5240 <isom5240@ggmail.ust.hk>
2026-02-14 11:36:47 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,5 +1,18 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 import streamlit as st
 
-st.write("ISOM5240")
-st.write("ISOM5240")
+pipe = pipeline("text-generation", model="google/gemma-3-270m-it")
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+answer = pipe(messages)[0]['generated_text'][1]['content']
+
+st.write(answer)
+
+###########requirements.txt############
+streamlit
+transformers
+torch
+write("ISOM5240")
 
