349be880d5f4de047893ac308a58b74a3491df28
isom5240 <isom5240@ggmail.ust.hk>
2026-02-28 17:38:19 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,27 +1,50 @@
+
+# import part
 from transformers import pipeline
 from PIL import Image
 import streamlit as st
 
-# Streamlit UI
-st.header("Title: Age Classification using ViT")
 
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification", 
-                          model="prithivMLmods/Age-Classification-SigLIP2")
+# function part
+def ageClassifier():
+    # Load the age classification pipeline
+    # The code below should be placed in the main part of the program
+    age_classifier = pipeline("image-classification", 
+                              model="prithivMLmods/Age-Classification-SigLIP2")
+    
+    image_name = "middleagedMan.jpg"
+    image_name = Image.open(image_name).convert("RGB")
+    
+    # Classify age
+    age_predictions = age_classifier(image_name)
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-st.write(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-
-# Display results
-st.write("Predicted Age Range:")
-st.write(f"Age range: {age_predictions[0]['label']}")
+    return age_predictions
 
 
 
-st.write("Done")
+def main():
+    # Streamlit UI
+    st.header("Title: Age Classification using ViT")
+    
+
+
+    age_predictions = ageClassifier()
+
+
+
+  
+    st.write(age_predictions)
+    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+    
+    # Display results
+    st.write("Predicted Age Range:")
+    st.write(f"Age range: {age_predictions[0]['label']}")
+    
+    st.write("Done")
+
+
+# main part
+if __name__ == "__main__":
+    main()
+
+
