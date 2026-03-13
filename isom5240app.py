68d705e4cb1962d9711ade704a7fa5bfc54473d3
isom5240 <isom5240@ggmail.ust.hk>
2026-03-13 23:57:04 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,68 +1,30 @@
 
-# Program title: Storytelling App
-
-# import part
-import streamlit as st
-from transformers import pipeline
-
-# function part
-# img2text
-def img2text(url):
-    image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-    text = image_to_text_model(url)[0]["generated_text"]
-    return text
-
-# text2story
-def text2story(text):
-    pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
-    story_text = pipe(text)[0]['generated_text']
-    return story_text
-
-# text2audio
-def text2audio(story_text):
-    pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
-    audio_data = pipe(story_text)
-    return audio_data
 
 
-def main():
-    st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
-    st.header("Turn Your Image to Audio Story")
-    uploaded_file = st.camera_input("Take a picture")
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import torch
+import numpy as np
 
-    if uploaded_file is not None:
-        print(uploaded_file)
-        bytes_data = uploaded_file.getvalue()
-        with open(uploaded_file.name, "wb") as file:
-            file.write(bytes_data)
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+# Testing with the saved model
+model2 = AutoModelForSequenceClassification.from_pretrained("CustomModel_yelp",
+                                                            num_labels=5)
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+# Tokenized testing data
+label = 4 # label = 4
+text = "dr. goldberg offers everything i look for in a general practitioner. he's nice and easy to talk to without being patronizing; he's always on time in seeing his patients; he's affiliated with a top-notch hospital (nyu) which my parents have explained to me is very important in case something happens and you need surgery; and you can get referrals to see specialists without having to see him first. really, what more do you need? i'm sitting here trying to think of any complaints i have about him, but i'm really drawing a blank."
+inputs = tokenizer(text,
+                   padding=True,
+                   truncation=True,
+                   return_tensors='pt')
 
-        #Stage 1: Image to Text
-        st.text('Processing img2text...')
-        scenario = img2text(uploaded_file.name)
-        st.write(scenario)
+outputs = model2(**inputs)
+predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+predictions = predictions.cpu().detach().numpy()
 
-        #Stage 2: Text to Story
-        st.text('Generating a story...')
-        story = text2story(scenario)
-        st.write(story)
+# Get the index of the largest output value
+max_index = np.argmax(predictions)
 
-        #Stage 3: Story to Audio data
-        st.text('Generating audio data...')
-        audio_data =text2audio(story)
+print(f"The label is {label} and the predicted label is {max_index}")
 
-        # Play button
-        if st.button("Play Audio"):
-            # Get the audio array and sample rate
-            audio_array = audio_data["audio"]
-            sample_rate = audio_data["sampling_rate"]
-
-            # Play audio directly using Streamlit
-            st.audio(audio_array,
-                     sample_rate=sample_rate)
-
-
-if __name__ == "__main__":
-    main()
-    
