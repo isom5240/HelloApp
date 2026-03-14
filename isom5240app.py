658000a42cf8a4abbc6fea7b4cf4a6efb803c6bd
isom5240 <isom5240@ggmail.ust.hk>
2026-03-14 15:39:27 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -27,6 +27,6 @@ predictions = predictions.cpu().detach().numpy()
 # Get the index of the largest output value
 max_index = np.argmax(predictions)
 
-st.header("Testing App")
+st.title("Testing App")
 #print(f"The label is {label} and the predicted label is {max_index}")
 st.write(f"The label is {label} and the predicted label is {max_index}")
